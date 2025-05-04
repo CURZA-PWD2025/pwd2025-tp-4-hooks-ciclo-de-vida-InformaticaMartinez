3013f6b1f92d27d6b90de71d8272360e262b00d6
InformaticaMartinez <julionicolasmartinez@gmail.com>
2025-05-04 13:56:21 -0300

    TP_4

---
+++ src/components/DimensionComponente.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="dimension-container">
+    <h2>Dimensiones del Componente</h2>
+    <p>Ancho: {{ width }}px</p>
+    <p>Alto: {{ height }}px</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const width = ref(0);
+const height = ref(0);
+
+const actualizarDimensiones = () => {
+  width.value = window.innerWidth;
+  height.value = window.innerHeight;
+};
+
+onMounted(() => {
+  actualizarDimensiones();
+  window.addEventListener('resize', actualizarDimensiones);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', actualizarDimensiones);
+});
+</script>
+
+<style scoped>
+.dimension-container {
+  background: rgb(70, 69, 69);
+  padding: 1.5rem;
+  border-radius: 16px;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s ease;
+  text-align: center;
+  width: 100%;
+  max-width: 300px;
+  margin: 1rem auto;
+}
+.dimension-container:hover {
+  transform: scale(1.03);
+}
+</style>
---
+++ src/components/Lista.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="lista-container">
+    <h2>Lista de Profesionales</h2>
+    <ul v-if="profesionales.length > 0">
+      <li v-for="profesional in profesionales" :key="profesional.id">
+        <strong>{{ profesional.nombre }}</strong>: {{ profesional.descripcion }}
+      </li>
+    </ul>
+    <p v-else class="cargando">Cargando profesionales...</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { profesiones } from '../resource/profesiones.ts';
+
+const profesionales = ref([]);
+
+onMounted(() => {
+  setTimeout(() => {
+    profesionales.value = profesiones;
+  }, 3000);
+});
+</script>
+
+<style scoped>
+.list-container {
+  background: rgb(70, 69, 69);
+  padding: 1.5rem;
+  border-radius: 16px;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s ease;
+  text-align: center;
+  width: 100%;
+  max-width: 300px;
+  margin: 1rem auto;
+  
+}
+
+
+h2 {
+  color: darkgoldenrod;
+  margin-bottom: 15px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  background: rgb(70, 69, 69);
+  padding: 1.5rem;
+  border-radius: 16px;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s ease;
+  text-align: center;
+  width: 100%;
+  max-width: 300px;
+  margin: 1rem auto;
+}
+li:hover {
+  transform: scale(1.03);
+}
+li strong {
+  color: darkgoldenrod;
+}
+
+.cargando {
+  text-align: center;
+  padding: 20px;
+  color: darkgoldenrod;
+  font-style: italic;
+}
+</style>
